009780ba775eb3cf9d9806d0ca8d2ef28e83ece2
Pebbili-Uma <umapebbili1996.Up@gmail.com>
2021-07-02 17:53:54 +0000

    update gadgets page code

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+module.exports = app.get("/", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+
+app.listen(3000);
